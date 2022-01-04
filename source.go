5b34b3a64ca5b40579479fa18e9ccf0de1b1a4b4
Florian Zwoch <fzwoch@gmail.com>
2022-01-04 15:32:09 +0100

    source: refactor a bit

--- source.go
+++ source.go
@@ -254,179 +254,175 @@ func source_loop(h *teleportSource) {
 		}
 
 		for {
-			h.Lock()
-			service, ok := h.services[teleport]
-			h.Unlock()
-
-			if ok {
-				var (
-					err        error
-					videoCount uint64
-					audioCount uint64
-				)
-
-				connMutex.Lock()
-
-				if shutdown {
-					connMutex.Unlock()
-
-					goto wait
-				}
-
-				if c != nil {
-					c.Close()
-					c = nil
-				}
-
-				c, err = net.DialTimeout("tcp", service.address+":"+strconv.Itoa(service.port), time.Second)
-
-				connMutex.Unlock()
-
-				if err != nil {
-					log.Println(err)
-					if !errors.Is(err, os.ErrDeadlineExceeded) {
-						time.Sleep(time.Second)
-					}
-					goto wait
-				}
-
-				j, err := json.Marshal(&options{
-					Quality: int(quality),
-				})
-				if err != nil {
-					log.Println(err)
-					goto wait
-				}
-
-				b := bytes.Buffer{}
-
-				binary.Write(&b, binary.LittleEndian, &options_header{
-					Magic: [4]byte{'O', 'P', 'T', 'S'},
-					Size:  int32(len(j)),
-				})
-
-				buffers := net.Buffers{
-					b.Bytes(),
-					j,
-				}
-
-				_, err = buffers.WriteTo(c)
-				if err != nil {
-					log.Println(err)
-					goto wait
-				}
-
-				for {
-					var header header
-
-					err = binary.Read(c, binary.LittleEndian, &header)
-					if err != nil {
-						log.Println(err)
-						goto wait
-					}
-					switch header.Type {
-					case [4]byte{'J', 'P', 'E', 'G'}:
-					case [4]byte{'W', 'A', 'V', 'E'}:
-					default:
-						panic("UNKNOWN HEADER TYPE")
-					}
-
-					b := make([]byte, header.Size)
-
-					_, err := io.ReadFull(c, b)
-					if err != nil {
-						log.Println(err)
-						goto wait
-					}
-
-					switch header.Type {
-					case [4]byte{'J', 'P', 'E', 'G'}:
-						info := &imageInfo{
-							timestamp: header.Timestamp,
-							b:         b,
-						}
-
-						h.imageLock.Lock()
-						h.images = append(h.images, info)
-						h.imageLock.Unlock()
-
-						h.Add(1)
-
-						go func(info *imageInfo) {
-							defer h.Done()
-
-							reader := bytes.NewReader(info.b)
-
-							img, err := jpeg.Decode(reader, &jpeg.DecoderOptions{})
-							if err != nil {
-								panic(err)
-							}
-							info.image = img.(*image.YCbCr)
-
-							h.imageLock.Lock()
-							defer h.imageLock.Unlock()
-
-							info.done = true
-
-							for len(h.images) > 0 && h.images[0].done {
-								h.frame.width = C.uint(h.images[0].image.Bounds().Dx())
-								h.frame.height = C.uint(h.images[0].image.Bounds().Dy())
-								h.frame.format = C.VIDEO_FORMAT_I420
-								h.frame.timestamp = C.uint64_t(h.images[0].timestamp)
-								h.frame.linesize[0] = C.uint(h.images[0].image.YStride)
-								h.frame.linesize[1] = C.uint(h.images[0].image.CStride)
-								h.frame.linesize[2] = C.uint(h.images[0].image.CStride)
-								h.frame.data[0] = (*C.uint8_t)(unsafe.Pointer(&h.images[0].image.Y[0]))
-								h.frame.data[1] = (*C.uint8_t)(unsafe.Pointer(&h.images[0].image.Cb[0]))
-								h.frame.data[2] = (*C.uint8_t)(unsafe.Pointer(&h.images[0].image.Cr[0]))
-
-								settings := C.obs_source_get_settings(h.source)
-								if C.obs_data_get_bool(settings, C.CString("ignore_timestamps")) {
-									h.frame.timestamp = C.uint64_t(videoCount)
-									videoCount++
-
-								}
-								C.obs_data_release(settings)
-
-								C.obs_source_output_video(h.source, h.frame)
-
-								h.frame.data[0] = nil
-								h.frame.data[1] = nil
-								h.frame.data[2] = nil
-
-								h.images = h.images[1:]
-							}
-						}(info)
-					case [4]byte{'W', 'A', 'V', 'E'}:
-						h.audio.timestamp = C.uint64_t(header.Timestamp)
-						h.audio.samples_per_sec = 48000
-						h.audio.speakers = C.SPEAKERS_STEREO
-						h.audio.format = C.AUDIO_FORMAT_16BIT
-						h.audio.frames = C.uint(header.Size) / 4
-						h.audio.data[0] = (*C.uint8_t)(unsafe.Pointer(&b[0]))
-
-						settings := C.obs_source_get_settings(h.source)
-						if C.obs_data_get_bool(settings, C.CString("ignore_timestamps")) {
-							h.audio.timestamp = C.uint64_t(audioCount)
-							audioCount++
-						}
-						C.obs_data_release(settings)
-
-						C.obs_source_output_audio(h.source, h.audio)
-
-						h.audio.data[0] = nil
-					}
-				}
-			} else {
-				time.Sleep(time.Second)
-			}
-
-		wait:
 			select {
 			case <-dial:
 				return
 			default:
-				C.obs_source_output_video(h.source, nil)
+			}
+
+			C.obs_source_output_video(h.source, nil)
+
+			h.Lock()
+			service, ok := h.services[teleport]
+			h.Unlock()
+
+			if !ok {
+				time.Sleep(time.Second)
+				continue
+			}
+
+			var (
+				err        error
+				videoCount uint64
+				audioCount uint64
+			)
+
+			connMutex.Lock()
+			if shutdown {
+				connMutex.Unlock()
+				return
+			}
+
+			if c != nil {
+				c.Close()
+			}
+			c, err = net.DialTimeout("tcp", service.address+":"+strconv.Itoa(service.port), time.Second)
+			connMutex.Unlock()
+
+			if err != nil {
+				log.Println(err)
+				if !errors.Is(err, os.ErrDeadlineExceeded) {
+					time.Sleep(time.Second)
+				}
+				continue
+			}
+
+			j, err := json.Marshal(&options{
+				Quality: int(quality),
+			})
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			b := bytes.Buffer{}
+
+			binary.Write(&b, binary.LittleEndian, &options_header{
+				Magic: [4]byte{'O', 'P', 'T', 'S'},
+				Size:  int32(len(j)),
+			})
+
+			buffers := net.Buffers{
+				b.Bytes(),
+				j,
+			}
+
+			_, err = buffers.WriteTo(c)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			for {
+				var header header
+
+				err = binary.Read(c, binary.LittleEndian, &header)
+				if err != nil {
+					log.Println(err)
+					break
+				}
+				switch header.Type {
+				case [4]byte{'J', 'P', 'E', 'G'}:
+				case [4]byte{'W', 'A', 'V', 'E'}:
+				default:
+					panic("UNKNOWN HEADER TYPE")
+				}
+
+				b := make([]byte, header.Size)
+
+				_, err := io.ReadFull(c, b)
+				if err != nil {
+					log.Println(err)
+					break
+				}
+
+				switch header.Type {
+				case [4]byte{'J', 'P', 'E', 'G'}:
+					info := &imageInfo{
+						timestamp: header.Timestamp,
+						b:         b,
+					}
+
+					h.imageLock.Lock()
+					h.images = append(h.images, info)
+					h.imageLock.Unlock()
+
+					h.Add(1)
+
+					go func(info *imageInfo) {
+						defer h.Done()
+
+						reader := bytes.NewReader(info.b)
+
+						img, err := jpeg.Decode(reader, &jpeg.DecoderOptions{})
+						if err != nil {
+							panic(err)
+						}
+						info.image = img.(*image.YCbCr)
+
+						h.imageLock.Lock()
+						defer h.imageLock.Unlock()
+
+						info.done = true
+
+						for len(h.images) > 0 && h.images[0].done {
+							h.frame.width = C.uint(h.images[0].image.Bounds().Dx())
+							h.frame.height = C.uint(h.images[0].image.Bounds().Dy())
+							h.frame.format = C.VIDEO_FORMAT_I420
+							h.frame.timestamp = C.uint64_t(h.images[0].timestamp)
+							h.frame.linesize[0] = C.uint(h.images[0].image.YStride)
+							h.frame.linesize[1] = C.uint(h.images[0].image.CStride)
+							h.frame.linesize[2] = C.uint(h.images[0].image.CStride)
+							h.frame.data[0] = (*C.uint8_t)(unsafe.Pointer(&h.images[0].image.Y[0]))
+							h.frame.data[1] = (*C.uint8_t)(unsafe.Pointer(&h.images[0].image.Cb[0]))
+							h.frame.data[2] = (*C.uint8_t)(unsafe.Pointer(&h.images[0].image.Cr[0]))
+
+							settings := C.obs_source_get_settings(h.source)
+							if C.obs_data_get_bool(settings, C.CString("ignore_timestamps")) {
+								h.frame.timestamp = C.uint64_t(videoCount)
+								videoCount++
+
+							}
+							C.obs_data_release(settings)
+
+							C.obs_source_output_video(h.source, h.frame)
+
+							h.frame.data[0] = nil
+							h.frame.data[1] = nil
+							h.frame.data[2] = nil
+
+							h.images = h.images[1:]
+						}
+					}(info)
+				case [4]byte{'W', 'A', 'V', 'E'}:
+					h.audio.timestamp = C.uint64_t(header.Timestamp)
+					h.audio.samples_per_sec = 48000
+					h.audio.speakers = C.SPEAKERS_STEREO
+					h.audio.format = C.AUDIO_FORMAT_16BIT
+					h.audio.frames = C.uint(header.Size) / 4
+					h.audio.data[0] = (*C.uint8_t)(unsafe.Pointer(&b[0]))
+
+					settings := C.obs_source_get_settings(h.source)
+					if C.obs_data_get_bool(settings, C.CString("ignore_timestamps")) {
+						h.audio.timestamp = C.uint64_t(audioCount)
+						audioCount++
+					}
+					C.obs_data_release(settings)
+
+					C.obs_source_output_audio(h.source, h.audio)
+
+					h.audio.data[0] = nil
+				}
 			}
 		}
 	}()
