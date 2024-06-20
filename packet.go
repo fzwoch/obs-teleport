7fe7488faaf315c849aeb0a88e43d13ba782ea59
Florian Zwoch <fzwoch@gmail.com>
2024-06-20 11:35:05 +0200

    add missing copyright headers

--- packet.go
+++ packet.go
@@ -1,3 +1,23 @@
+//
+// obs-teleport. OBS Studio plugin.
+// Copyright (C) 2021-2024 Florian Zwoch <fzwoch@gmail.com>
+//
+// This file is part of obs-teleport.
+//
+// obs-teleport is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// obs-teleport is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with obs-teleport. If not, see <http://www.gnu.org/licenses/>.
+//
+
 package main
 
 //
